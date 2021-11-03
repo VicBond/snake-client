073edd798f2659345d9d86c70e52f95982f8c18f
Victor Bondarenko <nirasimha@gmail.com>
2021-11-03 18:17:42 +0000

    Experiments added and commented after successful try

--- client.js
+++ client.js
@@ -4,24 +4,44 @@ const connect = function() {
   const conn = net.createConnection({
     host: '165.227.47.243', // IP address here,
     port: '50541',// PORT number here,
-    //IP: 165.227.47.243
-    // PORT: 50541
+  
+  });
+  const name = "OPP";
+  conn.setEncoding("utf8");
+  
+  conn.on("data", (data) => {
+    console.log("server says:", data);
   });
 
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    console.log(`Name: ${name}`);
+    // setTimeout(function() {
+    //   conn.write("Move: down");
+    // }, 50);
+    // setTimeout(function() {
+    //   conn.write("Move: left");
+    // }, 50);
+    // setInterval(function () {
+    //   conn.write("Move: up");
+    // }, 50);
+    // conn.write("Move: left");
+    // conn.write("Move: down");
+  });
   // interpret incoming data as text
-  conn.setEncoding("utf8");
+  // stdin.setEncoding('utf8');
 
   conn.on("event name", () => {
     // code that does something
-    // client.write('Move: Up');
+    // conn.on('Name: Sneaky');
    
   });
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-    // client.write('Name: Sneaky');
-  });
+
+  // stdin.on('data', (input) => {
+  //   conn.write(`${name}: ${input}`);
+  // });
 
   return conn;
 };
 
-module.exports = connect;
+module.exports = {connect};
--- play.js
+++ play.js
@@ -1,5 +1,4 @@
-//const net = require("net");
-const connect = require("./client");
+const {connect} = require("./client");
 // establishes a connection with the game server
 
 
