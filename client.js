9c59f10cb8f77d2bf1b1a457261802cb50663fcf
Victor Bondarenko <nirasimha@gmail.com>
2021-11-03 23:52:10 +0000

    input file created with two functions which are moved from play.js

--- client.js
+++ client.js
@@ -2,39 +2,33 @@ const net = require("net");
 
 const connect = function() {
   const conn = net.createConnection({
-    host: '165.227.47.243', // IP address here,
-    port: '50541',// PORT number here,
+    host: '165.227.47.243',
+    port: '50541',
   
   });
+
   const name = "OPP";
   conn.setEncoding("utf8");
   
-
   conn.on("data", (data) => {
     console.log("server says:", data);
   });
 
   conn.on("connect", () => {
-    // code that does something when the connection is first established
     console.log(`Name: ${name}`);
     console.log(`Server Says: `,'successfully connected to the snake server');
   
   });
-  // interpret incoming data as text
+
 
 
   conn.on("event name", () => {
-    // code that does something
-   
+ 
     
   });
    
 
-  // stdin.on('data', (input) => {
-  //   conn.write(`${name}: ${input}`);
-  // });
-
   return conn;
 };
 
-module.exports = {connect};
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,24 +1,9 @@
-const {connect} = require("./client");
-// establishes a conn with the game server
-
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-
-};
-
-const setupInput = function(conn) {
-  connection = conn;
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 
 console.log("Connecting ...");
-setupInput();
-connect();
+
+setupInput()
+
+connect()
